2af1af2b27985b33970759f0a727e136ef0e40b2
kn06497 <kn06497@hotmail.com>
2017-01-11 04:44:06 +0000

    fix
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Jun 4 14:58:29 KST 2015"
-#define LINUX_COMPILE_BY "egrace.hwang"
-#define LINUX_COMPILE_HOST "eunhye-Samsung-DeskTop-System"
-#define LINUX_COMPILER "gcc version 4.8 (GCC) "
